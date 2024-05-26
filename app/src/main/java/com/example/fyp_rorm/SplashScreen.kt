d1d601193c7f5f44d1fb641be73231b3e651cf99
tansh <tanshuening@gmail.com>
2024-05-27 06:26:22 +0800

    Create Splash Screen

--- app/src/main/java/com/example/fyp_rorm/SplashScreen.kt
+++ app/src/main/java/com/example/fyp_rorm/SplashScreen.kt
@@ -3,18 +3,18 @@ package com.example.fyp_rorm
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
             finish()
