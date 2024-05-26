29b9ac9f66b73d2c4b60e9a75557a688c442775e
tansh <tanshuening@gmail.com>
2024-05-27 06:26:13 +0800

    Create Login

--- app/src/main/java/com/example/fyp_rorm/LoginActivity.kt
+++ app/src/main/java/com/example/fyp_rorm/LoginActivity.kt
@@ -1,20 +1,31 @@
 package com.example.fyp_rorm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fyp_rorm.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding:ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignActivity::class.java)
+            startActivity(intent)
         }
+
+        binding.createAccount.setOnClickListener {
+            val intent = Intent(this, SignActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
