fc5715fabfd7ecbc228961a8c05a9d5af745b371
tansh <tanshuening@gmail.com>
2024-05-27 06:26:01 +0800

    Create Sign Up

--- app/src/main/java/com/example/fyp_rorm/SignActivity.kt
+++ app/src/main/java/com/example/fyp_rorm/SignActivity.kt
@@ -1,20 +1,31 @@
 package com.example.fyp_rorm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fyp_rorm.databinding.ActivityLoginBinding
+import com.example.fyp_rorm.databinding.ActivitySignBinding
 
 class SignActivity : AppCompatActivity() {
+
+    private val binding: ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+/*        binding.signUpButton.setOnClickListener {
+            val intent = Intent(this, SignActivity::class.java)
+            startActivity(intent)
+        }*/
+
+        binding.alreadyAccount.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
