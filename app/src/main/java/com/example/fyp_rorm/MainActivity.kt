5e08d772229c70428f6211b5ce683eaa55f03bd8
tansh <tanshuening@gmail.com>
2024-05-28 16:34:36 +0800

    Create Home Page

--- app/src/main/java/com/example/fyp_rorm/MainActivity.kt
+++ app/src/main/java/com/example/fyp_rorm/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fyp_rorm
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,12 +10,26 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.fyp_rorm.databinding.ActivityChooseLocationBinding
+import com.example.fyp_rorm.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+/*
         setContentView(R.layout.activity_main)
+*/
+
+        setContentView(binding.root)
+        val locationList: Array<String> = arrayOf("Kuala Lumpur", "Penang", "Johor", "Kelantan")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView: AutoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
 
         var NavController: NavController = findNavController(R.id.fragmentContainerView)
         var bottomnav = findViewById<BottomNavigationView>(R.id.navigationBar)
