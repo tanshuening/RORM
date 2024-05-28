1fca288e7b691e86c409f673b01d3ca8ed0b7697
tansh <tanshuening@gmail.com>
2024-05-28 13:48:52 +0800

    Create Banner

--- app/src/main/java/com/example/fyp_rorm/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fyp_rorm/Fragment/HomeFragment.kt
@@ -1,16 +1,16 @@
 package com.example.fyp_rorm.Fragment
 
-import android.icu.number.Scale
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.fyp_rorm.R
 import com.example.fyp_rorm.databinding.FragmentHomeBinding
-
 
 class HomeFragment : Fragment() {
 
@@ -18,7 +18,6 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -34,15 +33,21 @@ class HomeFragment : Fragment() {
 
         val imageList = ArrayList<SlideModel>()
         imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
-        imageList.add(SlideModel(R.drawable.banner2, ScaleTypes.FIT))
-        imageList.add(SlideModel(R.drawable.banner3, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
 
         val imageSlider = binding.imageSlider
         imageSlider.setImageList(imageList)
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
-    }
+        imageSlider.setItemClickListener(object: ItemClickListener {
+            override fun doubleClick(position: Int) {
+                // Implement double click action if needed
+            }
 
-    companion object {
-
+            override fun onItemSelected(position: Int) {
+                val itemMessage = "Selected Image $position"
+                Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
